db77f0db49141296fb6934ab02b591a8870c232c
Tom Shafer <tj@tjshafer.com>
2016-09-02 14:38:13 -0700

    privacy policy
    Signed-off-by: Tom Shafer <tj@tjshafer.com>

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -6,31 +6,222 @@
     <div class="container main-container">
         <div class="row">
             <div class="col-md-12 hero-feature">
-                <span class="title">PRIVACY POLICY</span>
-                <h4>Lorem ipsum dolor sit amet conse ctetur adipisicing elit</h4>
-                <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.</p>
+                <span class="title">spacewalksales.com Privacy Policy</span>
+
+                <p>This privacy policy has been compiled to better serve those who are concerned with how their
+                    'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy
+                    law and information security, is information that can be used on its own or with other information
+                    to identify, contact, or locate a single person, or to identify an individual in context. Please
+                    read our privacy policy carefully to get a clear understanding of how we collect, use, protect or
+                    otherwise handle your Personally Identifiable Information in accordance with our website.</p>
+
+                <h4>What personal information do we collect from the people that visit our blog, website or app?</h4>
+
+                <p>When ordering or registering on our site, as appropriate, you may be asked to enter your name, email
+                    address, mailing address, phone number or other details to help you with your experience.</p>
+
+                <h4>When do we collect information?</h4>
+
+                <p>We collect information from you when you place an order, fill out a form or enter information on our
+                    site.</p>
+
+                <h4>How do we use your information? </h4>
+
+                <p>We may use the information we collect from you when you register, make a purchase, sign up for our
+                    newsletter, respond to a survey or marketing communication, surf the website, or use certain other
+                    site features in the following ways:</p>
+
                 <ul>
-                    <li>Lorem ipsum dolor sit amet</li>
-                    <li>Conse ctetur</li>
-                    <li>Aadipisicing elit</li>
-                    <li>Sed do eiusmod tempor</li>
+                    <li>To personalize your experience and to allow us to deliver the type of content and product
+                        offerings in which you are most interested.
+                    </li>
+                    <li>To allow us to better service you in responding to your customer service requests.</li>
+                    <li>To send periodic emails regarding your order or other products and services.</li>
                 </ul>
-                <h4>Dolor sit amet conse ctetur adipisicing elit</h4>
-                <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-                    eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.</p>
+
+                <h4>How do we protect your information?</h4>
+
+                <p>Our website is scanned on a regular basis for security holes and known vulnerabilities in order to
+                    make your visit to our site as safe as possible.</p>
+
+                <h4>We use regular Malware Scanning.</h4>
+
+                <p>Your personal information is contained behind secured networks and is only
+                    accessible by a limited number of persons who have special access rights to such systems, and
+                    are required to keep the information confidential. In addition, all sensitive/credit information
+                    you supply is encrypted via Secure Socket Layer (SSL) technology.</p>
+
+                <p>We implement a variety of security measures when a user enters, submits, or
+                    accesses their information to maintain the safety of your personal information.</p>
+
+                <p>All transactions are processed through a gateway provider and are not stored
+                    or processed on our servers.</p>
+
+                <h4>Do we use 'cookies'?</h4>
+                <p>Yes. Cookies are small files that a site or its service provider transfers to
+                    your computer's hard drive through your Web browser (if you allow) that enables the site's or
+                    service provider's systems to recognize your browser and capture and remember certain
+                    information. For instance, we use cookies to help us remember and process the items in your
+                    shopping cart. They are also used to help us understand your preferences based on previous or
+                    current site activity, which enables us to provide you with improved services. We also use
+                    cookies to help us compile aggregate data about site traffic and site interaction so that we can
+                    offer better site experiences and tools in the future.
+                </p>
+
+                <p>
+                    <b>We use cookies to:</b>
+                    <ol>
+                        <li>Help remember and process the items in the shopping cart.</li>
+                    </ol>
+                </p>
+
+                <p>You can choose to have your computer warn you each time a cookie is being
+                    sent, or you can choose to turn off all cookies. You do this through your browser settings.
+                    Since browser is a little different, look at your browser's Help Menu to learn the correct way
+                    to modify your cookies.
+                </p>
+
+                <b>If users disable cookies in their browser:</b>
+
+                <p>If you turn cookies off, some features will be disabled. Some of the features
+                    that make your site experience more efficient and may not function properly, however, you will still
+                    be
+                    able to place orders items in cart.</p>
+
+                <h4>Third-party disclosure</h4>
+                <p>We do not sell, trade, or otherwise transfer to outside parties your
+                    Personally Identifiable Information.</p>
+
+                <h4>Third-party links</h4>
+                <p>We do not include or offer third-party products or services on our website.</p>
+                <p><b>Google</b></p>
+                <p>Google's advertising requirements can be summed up by Google's Advertising
+                    Principles. They are put in place to provide a positive experience for users.
+                    <a href="https://support.google.com/adwordspolicy/answer/1316548?hl=en" target="_blank">https://support.google.com/adwordspolicy/answer/1316548?hl=en</a>
+                </p>
+                <p>We use Google AdSense Advertising on our website.</p>
+                <p>Google, as a third-party vendor, uses cookies to serve ads on our site.
+                    Google's use of the DART cookie enables it to serve ads to our users based on previous visits to
+                    our site and other sites on the Internet. Users may opt-out of the use of the DART cookie by
+                    visiting the Google Ad and Content Network privacy policy.</p>
+                <p>
+                    <b>We have implemented the following:</b>
+                <ul>
+                    <li>Remarketing with Google AdSense</li>
+                    <li>Demographics and Interests Reporting</li>
+                </ul>
+                </p>
+                <p>We, along with third-party vendors such as Google use first-party cookies
+                    (such as the Google Analytics cookies) and third-party cookies (such as the DoubleClick cookie)
+                    or other third-party identifiers together to compile data regarding user interactions with ad
+                    impressions and other ad service functions as they relate to our website.
+                    <b>Opting out:</b>
+                <p>Users can set preferences for how Google advertises to you using the Google Ad Settings
+                    page. Alternatively, you can opt out by visiting the Network Advertising Initiative Opt
+                    Out page or by using the Google Analytics Opt Out Browser add on.</p>
+
+                <h4>California Online Privacy Protection Act</h4>
+                <p>CalOPPA is the first state law in the nation to require commercial websites
+                    and online services to post a privacy policy. The law's reach stretches well beyond California
+                    to require any person or company in the United States (and conceivably the world) that operates
+                    websites collecting Personally Identifiable Information from California consumers to post a
+                    conspicuous privacy policy on its website stating exactly the information being collected and
+                    those individuals or companies with whom it is being shared. - See more at:
+                    http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf</p>
+                <p><b>According to CalOPPA, we agree to the following:</b>
+                <p>Users can visit our site anonymously.</p>
+                <p>Once this privacy policy is created, we will add a link to it on our home
+                    page or as a minimum, on the first significant page after entering our website.</p>
+                <p>Our Privacy Policy link includes the word 'Privacy' and can be easily be
+                    found on the page specified above.</p>
+
+                <b>You will be notified of any Privacy Policy changes:</b>
+                <ul>
+                    <li> On our Privacy Policy Page
+                </ul>
+                <b>Can change your personal information:</b>
+                <ul>
+                    <li>By emailing us</li>
+                    <li>By calling us</li>
+                </ul>
+                <p><b>How does our site handle Do Not Track signals?</b></p>
+                <p>We honor Do Not Track signals and Do Not Track, plant cookies, or use
+                    advertising when a Do Not Track (DNT) browser mechanism is in place.
+                </p>
+                <p>Does our site allow third-party behavioral tracking?</p>
+                <p>It's also important to note that we allow third-party behavioral tracking</p>
+
+                <h4>COPPA (Children Online Privacy Protection Act)</h4>
+
+                <p>When it comes to the collection of personal information from children under
+                    the age of 13 years old, the Children's Online Privacy Protection Act (COPPA) puts parents in
+                    control. The Federal Trade Commission, United States' consumer protection agency, enforces the
+                    COPPA Rule, which spells out what operators of websites and online services must do to protect
+                    children's privacy and safety online.</p>
+                <p>We do not specifically market to children under the age of 13 years old.</p>
+
+                <h4>Fair Information Practices</h4>
+
+                <p>The Fair Information Practices Principles form the backbone of privacy law in
+                    the United States and the concepts they include have played a significant role in the
+                    development of data protection laws around the globe. Understanding the Fair Information
+                    Practice Principles and how they should be implemented is critical to comply with the various
+                    privacy laws that protect personal information.</p>
+                <p>In order to be in line with Fair Information Practices we will take
+                    the following responsive action, should a data breach occur:</p>
+                <p>We will notify you via email</p>
+                <ul>
+                    <li>Within 7 business days</li>
+                </ul>
+
+                <p>We also agree to the Individual Redress Principle which requires that
+                    individuals have the right to legally pursue enforceable rights against data collectors and
+                    processors who fail to adhere to the law. This principle requires not only that individuals have
+                    enforceable rights against data users, but also that individuals have recourse to courts or
+                    government agencies to investigate and/or prosecute non-compliance by data processors.</p>
+
+                <h4>CAN SPAM Act</h4>
+                <p>The CAN-SPAM Act is a law that sets the rules for commercial email,
+                    establishes requirements for commercial messages, gives recipients the right to have emails
+                    stopped from being sent to them, and spells out tough penalties for violations.</p>
+                <p><b>We collect your email address in order to:</b>
+                <ul>
+                    <li>Send information, respond to inquiries, and/or other requests or questions</li>
+                    <li>Process orders and to send information and updates pertaining to orders.</li>
+                    <li>Send you additional information related to your product and/or service</li>
+                    <li>Market to our mailing list or continue to send emails to our clients after the original
+                        transaction has occurred.
+                    </li>
+                </ul>
+                <p><b>To be in accordance with CANSPAM, we agree to the following:</b>
+                <ul>
+                    <li>Not use false or misleading subjects or email addresses.</li>
+                    <li>Identify the message as an advertisement in some reasonable way.</li>
+                    <li>Include the physical address of our business or site headquarters.</li>
+                    <li>Monitor third-party email marketing services for compliance, if one is used.</li>
+                    <li>Honor opt-out/unsubscribe requests quickly.</li>
+                    <li>Allow users to unsubscribe by using the link at the bottom of each email.</li>
+                </ul>
+                </p>
+                <p><b>If at any time you would like to unsubscribe from receiving future emails, you can email us at</b>
+                <ul>
+                    <li>Follow the instructions at the bottom of each email and we will promptly remove you from
+                        <b>ALL</b> correspondence.
+                    </li>
+                </ul>
+                </p>
+                <h4>Contacting Us</h4>
+                <p>If there are any questions regarding this privacy policy, you may contact us using the information
+                    below.</p>
+                <p>SpaceWalkSales.com<br/>
+                    450 31st St.<br/>
+                    Kenner, LA 70065<br/>
+                    United States<br/>
+                    info@herecomesfun.com<br/>
+                    800-622-6026</p>
+                <p>Last Edited on 2016-09-02</p>
             </div>
         </div>
     </div>
+
 @stop
