6a45ed773a14fa0077314a164d1ff25ed024f64b
Tom Shafer <tj@tjshafer.com>
2016-09-23 15:43:30 -0700

    update app url
    Signed-off-by: Tom Shafer <tj@tjshafer.com>

--- config/app.php
+++ config/app.php
@@ -35,7 +35,7 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
-    'url'             => env('APP_URL', 'http://localhost'),
+    'url'             => env('APP_URL', 'http://www.spacewalksales.com'),
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
