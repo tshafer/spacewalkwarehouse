9c5d4fb32bae36c0f46babfcf086865cac655507
Tom Shafer <tj@tjshafer.com>
2017-01-10 14:28:23 -0700

    email address

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -52,7 +52,7 @@ class CheckoutController extends Controller
             $message->subject('Space Walk Sales Requests Form');
             $message->from('sales@spacewalk.com', 'Space Walk Sales Request');
 
-            $message->to('admin@spacewalksales.com');
+            $message->to('​paul@herecomesfun.com');
 
         });
 
